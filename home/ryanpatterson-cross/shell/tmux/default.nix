5f2b165fc0cc58fa0a8d2d6a2c07c717812ba78d
rbpatt2019 <rb.patterson.cross@gmail.com>
2024-06-10 21:06:39 +0100

    fix(tmux): fix plugin opts
    Turns out that plugin configs need to be specified with the config to get them to load correctly

--- home/ryanpatterson-cross/shell/tmux/default.nix
+++ home/ryanpatterson-cross/shell/tmux/default.nix
@@ -10,9 +10,10 @@ let
       sha256 = "sha256-iUUKt63zz6ZC1wA6gMi6+hod7ldBNP4UigcdNQP09MU=";
     };
   };
-  power-zoom = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "power-zoom";
+  zoom = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "zoom";
     version = "2024-06-07";
+    rtpFilePath = "power-zoom.tmux";
     src = pkgs.fetchFromGitHub {
       owner = "jaclu";
       repo = "tmux-power-zoom";
@@ -27,6 +28,7 @@ in
 
     shortcut = "a";
 
+    sensibleOnTop = false;
     clock24 = true;
     keyMode = "vi";
     mouse = false;
@@ -34,9 +36,49 @@ in
     terminal = "xterm-256color";
 
     plugins = with pkgs.tmuxPlugins; [
+      # I would really prefer these to be in the conf file, but they need to be before
+      # the source to work, and that requires this option
       {
         plugin = catppuccin;
-        extraConfig = "set -g @catppuccin_flavour 'mocha'";
+        extraConfig = ''
+          set-option -g status-position top
+
+          set -g @catppuccin_window_left_separator "█"
+          set -g @catppuccin_window_right_separator "█ "
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_middle_separator "  █"
+
+          set -g @catppuccin_window_default_fill "number"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right "cpu battery date_time"
+          set -g @catppuccin_status_left_separator  ""
+          set -g @catppuccin_status_right_separator " "
+          set -g @catppuccin_status_fill "all"
+          set -g @catppuccin_status_connect_separator "yes"
+
+          set -g @catppuccin_cpu_color "#{thm_pink}"
+          set -g @catppuccin_battery_color "#{thm_green}"
+          set -g @catppuccin_date_time_color "#{thm_blue}"
+        '';
+      }
+      battery
+      cpu
+      {
+        plugin = menus;
+        extraConfig = ''
+          set -g @menus_location_x C
+          set -g @menus_location_y C
+        '';
+      }
+      {
+        plugin = zoom;
+        extraConfig = ''
+          unbind z
+          set -g @power_zoom_trigger 'z'
+        '';
       }
     ];
 
