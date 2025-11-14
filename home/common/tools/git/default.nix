8b74a4b3eac04910eabda0bdbd2f3c0ea9d17d05
rbpatt2019 <rb.patterson.cross@gmail.com>
2025-11-14 12:48:10 +0000

    refactor(git/default.nix): update git settings
    Update a number of git settings to get the errors about name changes to go away

--- home/common/tools/git/default.nix
+++ home/common/tools/git/default.nix
@@ -1,59 +1,49 @@
 {
   programs.git = {
     enable = true;
-    userName = "rbpatt2019";
-    userEmail = "rb.patterson.cross@gmail.com";
     ignores = [
       "*~"
       ".DS_Store"
       ".direnv"
       ".env"
       ".venv"
+      ".nvim-undodir"
     ];
-    aliases = {
-      a = "add";
-      bye = "clean -fdx";
-      b = "branch";
-      c = "cz";
-      d = "diff";
-      du = "diff @..@{u}";
-      ds = "diff --cached";
-      dl = "log --ext-diff -L";
-      df = "log -p --ext-diff --";
-      f = "fetch";
-      g = "merge";
-      l = "log --pretty=lc --graph";
-      lu = "log --pretty=lc --graph @..@{u}";
-      ll = "log --pretty=lf --graph";
-      la = "log --pretty=lc --graph --all";
-      p = "push";
-      pub = "push -u origin HEAD";
-      r = "reset";
-      rb = "rebase";
-      rbi = "rebase -i";
-      s = "status -sb";
-      sh = "show --ext-diff";
-      st = "stash";
-      stp = "stash pop";
-      stl = "stash list --pretty=rlo --stat";
-      sts = "stash show --ext-diff --patch";
-      sw = "switch";
-      swc = "switch --create";
-      swd = "switch --detach";
-      t = "tag";
-      undo = "restore";
-      x = "rm";
-    };
-    delta = {
-      enable = true;
-      options = {
-        side-by-side = true;
-        dark = true;
-        syntax-theme = "catppuccin";
-        hyperlinks = true;
+    settings = {
+      alias = {
+        a = "add";
+        bye = "clean -fdx";
+        b = "branch";
+        c = "cz";
+        d = "diff";
+        du = "diff @..@{u}";
+        ds = "diff --cached";
+        dl = "log --ext-diff -L";
+        df = "log -p --ext-diff --";
+        f = "fetch";
+        g = "merge";
+        l = "log --pretty=lc --graph";
+        lu = "log --pretty=lc --graph @..@{u}";
+        ll = "log --pretty=lf --graph";
+        la = "log --pretty=lc --graph --all";
+        p = "push";
+        pub = "push -u origin HEAD";
+        r = "reset";
+        rb = "rebase";
+        rbi = "rebase -i";
+        s = "status -sb";
+        sh = "show --ext-diff";
+        st = "stash";
+        stp = "stash pop";
+        stl = "stash list --pretty=rlo --stat";
+        sts = "stash show --ext-diff --patch";
+        sw = "switch";
+        swc = "switch --create";
+        swd = "switch --detach";
+        t = "tag";
+        undo = "restore";
+        x = "rm";
       };
-    };
-    extraConfig = {
       core = {
         editor = "nvim";
       };
@@ -78,6 +68,20 @@
         "ssh://git@github.com/".insteadOf = "https://github.com/";
         "ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
       };
+      user = {
+        name = "rbpatt2019";
+        email = "rb.patterson.cross@gmail.com";
+      };
+    };
+  };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      side-by-side = true;
+      dark = true;
+      syntax-theme = "catppuccin";
+      hyperlinks = true;
     };
   };
 
