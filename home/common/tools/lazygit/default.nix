e59a8a59fa8ec7c6642d726d841df7959b7f239e
rbpatt2019 <rb.patterson.cross@gmail.com>
2026-06-12 15:44:15 +0100

    fix(lazygit): migrate to new config

--- home/common/tools/lazygit/default.nix
+++ home/common/tools/lazygit/default.nix
@@ -3,10 +3,11 @@
     enable = true;
     settings = {
       git = {
-        paging = {
-          colorArg = "always";
-          pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format='lazygit-edit://{path}:{line}'";
-        };
+        pagers = [
+          {
+            pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format='lazygit-edit://{path}:{line}'";
+          }
+        ];
       };
     };
   };
